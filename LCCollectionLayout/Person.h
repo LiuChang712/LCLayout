1897b1294492d58d25bb9c1c9bf6d958fff9e733
LiuChang <986957270@qq.com>
2016-08-03 10:12:23 +0800

    first commit

---
+++ LCCollectionLayout/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  LCCollectionLayout
+//
+//  Created by 刘畅 on 16/8/1.
+//  Copyright © 2016年 LiuChang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ LCCollectionLayout/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  LCCollectionLayout
+//
+//  Created by 刘畅 on 16/8/1.
+//  Copyright © 2016年 LiuChang. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
